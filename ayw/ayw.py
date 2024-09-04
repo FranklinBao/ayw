ffa04e7fc02e99d2b3fbf396296ca1bae9564f14
FranklinBao <49803839+FranklinBao@users.noreply.github.com>
2024-09-05 00:27:26 +0800

    major bug fix & quote updates

--- ayw/ayw.py
+++ ayw/ayw.py
@@ -2,6 +2,7 @@ import sys
 import inspect
 import random
 import importlib.resources
+from types import FrameType
 
 if sys.argv[0] is not None and len(sys.argv[0]) > 0:
     with open(sys.argv[0]) as f:
@@ -21,12 +22,13 @@ class __MetaNB(type):
 class __BaseNB(metaclass=__MetaNB):
     good_import: bool = None
 
-    @staticmethod
-    def check_good_import(cls) -> bool:
+    @classmethod
+    def check_good_import(cls, caller_frame: FrameType = None) -> bool:
+        if caller_frame is None:
+            caller_frame = inspect.currentframe().f_back.f_back
         if cls.good_import is None:
-            cf = inspect.currentframe()
-            super_locals = cf.f_back.f_back.f_locals
-            if "nb" in super_locals.keys() and super_locals["nb"] is sys.modules[__package__]:
+            caller_locals = caller_frame.f_locals
+            if "nb" in caller_locals.keys() and caller_locals["nb"] is sys.modules[__package__]:
                 cls.good_import = True
             else:
                 cls.good_import = False
@@ -40,13 +42,13 @@ class _NB(__BaseNB, metaclass=__MetaNB):
             self.quotes = f.readlines()
 
     def __repr__(self):
-        if self.check_good_import(self.__class__):
+        if self.check_good_import():
             return random.choice(self.quotes)
         else:
             return "You don't import with respect. You don't offer friendship. You don't even think to call me \"NB\""
 
     def __call__(self):
-        self.check_good_import(self.__class__)
+        self.check_good_import(inspect.currentframe().f_back)
         print(self.__repr__())
 
 
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 DESCRIPTION = "This python module do nothing but print \"ayw NB\" as \"import ayw as nb\", and \"nb\" at simple import."
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 
 setup(name="ayw",
       version=VERSION,
       description=DESCRIPTION,
       url="https://github.com/FranklinBao/ayw",
       packages=find_packages(),
+      package_data={"ayw": ["resources/*"]},
       classifiers=[
             "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Developers"
